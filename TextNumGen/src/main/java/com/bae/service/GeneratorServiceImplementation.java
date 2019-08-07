db4a5bf435b3b1c0f578d3663e3a7d8fdf152cbc
Chris-McQuiggan <chris@mcquiggan.info>
2019-08-07 10:24:00 +0100

    updated

--- TextNumGen/src/main/java/com/bae/service/GeneratorServiceImplementation.java
+++ TextNumGen/src/main/java/com/bae/service/GeneratorServiceImplementation.java
@@ -15,22 +15,12 @@ public class GeneratorServiceImplementation {
 
 	public String numGen(int digits) {
 		SecureRandom random = new SecureRandom();
-			if (digits % 2 != 0) {
-				String num2 = String.format(
-						"%0"+(int)Math.floor(0.5 * digits)+"d", 
-						random.nextInt((int) Math.pow(10, Math.floor(0.5 * digits))));
-				String num1 = String.format(
-						"%0"+(int)(Math.floor(0.5 * digits)+1)+"d",
-						random.nextInt((int) Math.pow(10, (Math.floor(0.5 * digits) + 1))));
-				return num1 + num2;
-			} else {
-				String num2 = String.format(
-						"%0"+(int)Math.floor(0.5 * digits)+"d", 
-						random.nextInt((int) Math.pow(10, Math.floor(0.5 * digits))));
 				String num1 = String.format(
 						"%0"+(int)Math.floor(0.5 * digits)+"d", 
 						random.nextInt((int) Math.pow(10, Math.floor(0.5 * digits))));
+				String num2 = String.format(
+						"%0"+(int)(Math.floor((0.5 * digits)+0.5))+"d",
+						random.nextInt((int) Math.pow(10, Math.floor((0.5 * digits)+0.5))));
 				return num1 + num2;
-		}
 	}
 }
