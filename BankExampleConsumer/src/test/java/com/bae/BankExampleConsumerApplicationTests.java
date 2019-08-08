f45aa61db16fa6491fd15bd0b0f13d0ca479ae91
Chris-McQuiggan <chris@mcquiggan.info>
2019-08-08 12:09:11 +0100

    massive remake

--- BankExampleConsumer/src/test/java/com/bae/BankExampleConsumerApplicationTests.java
+++ BankExampleConsumer/src/test/java/com/bae/BankExampleConsumerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BankExConsumerApplicationTests {
+public class BankExampleConsumerApplicationTests {
 
 	@Test
 	public void contextLoads() {
